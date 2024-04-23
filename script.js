03f87b4da37af80df3dfe91311ac8c1d2e1c4bb5
WebDevRock <chriskirk1806@gmail.com>
2024-04-24 00:58:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+function init() {
+    const config = {
+        SVE: {
+            contractBase: 1875,
+            contractval: 0,
+            training: 0,
+            setup: 0,
+        },
+        Loop: {
+            contractBase: 14400,
+            contractval: 0,
+            training: 995,
+            setup: 495,
+        },
+        Global: {
+            contractBase: 15845,
+            contractval: 0,
+            training: 0,
+            setup: 0,
+        },
+        SocValEx: {
+            contractBase: 12000,
+            contractval: 0,
+            training: 0,
+            setup: 0,
+        },
+        SocValPort: {
+            contractBase: 18000,
+            contractval: 0,
+            training: 0,
+            setup: 0,
+        },
+        InHouse: {
+            contractBase: 24000,
+            contractval: 0,
+            training: 0,
+            setup: 0,
+        },
+        Thrive: {
+            contractBase: 40000,
+            contractval: 0,
+            training: 3000,
+            setup: 0,
+        },
+    };
+
+    const model = document.getElementById("model");
+    const contracts = document.getElementById("contracts");
+    const contractval = document.getElementById("contractval");
+    const training = document.getElementById("training");
+    const setup = document.getElementById("setup");
+    const calculator = document.getElementById("calc");
+    const totalCosts = document.getElementById("totalCosts");
+
+    // console.log(model.parentElement.parentElement.hidden=true);
+    // console.log(contracts);
+    // console.log(contractval);
+    // console.log(training);
+    // console.log(setup);
+
+    model.addEventListener("sl-change", () => {
+        if (config.hasOwnProperty(model.value) === false) {
+            console.log("Error property doesnt exist");
+            return;
+        }
+        const selectedModel = config[model.value];
+
+        // console.table(selectedModel);
+        contracts.value = selectedModel.contractBase
+        training.value = selectedModel.training
+        setup.value = selectedModel.setup
+        totalCosts.value = contracts.value + training.value + setup.value
+
+        for (const [key, value] of Object.entries(selectedModel)) {
+            // console.log(`Key: ${key}, Value: ${value}`);
+            if (typeof value == "object") {
+                for (const [xkey, xvalue] of Object.entries(value)) {
+                    console.log(key);
+                }
+            }
+        }
+
+    });
+
+    function setField(id, value) {}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+});
